450b5e38e040a420fe3bac1a82da7bebaa7498b5
Abdullah Al-Shamiry <its.shamiry@gmail.com>
2026-02-08 20:10:32 +0000

    feat: add utility for local IDs

---
+++ src/utils/local-id.js
@@ -0,0 +1,10 @@
+const STORAGE_KEY = 'echo-local-id';
+
+export function getLocalId() {
+  let id = localStorage.getItem(STORAGE_KEY);
+  if (!id) {
+    id = crypto.randomUUID();
+    localStorage.setItem(STORAGE_KEY, id);
+  }
+  return id;
+}
